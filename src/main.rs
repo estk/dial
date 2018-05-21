93d8570957b6bbc2b7174b4c8f11093f9f3d3f94
Evan Simmons <evan.simmons@creditkarma.com>
2018-05-21 13:24:07 -0700

    use impl Trait

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ extern crate futures;
 extern crate tokio;
 extern crate tokio_tcp;
 extern crate trust_dns_resolver;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
-use std::net::*;
 use std::io;
+use std::net::*;
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6, ToSocketAddrs};
 use std::time::{Duration, SystemTime};
 use std::vec;
@@ -13,25 +14,27 @@ use std::vec;
 // use futures::task::Context;
 // use futures::{Async, , Poll};
 
-use tokio::runtime::current_thread::Runtime;
 use tokio::prelude::*;
+use tokio::runtime::current_thread::Runtime;
 
+use trust_dns_resolver::error::{ResolveError, ResolveResult};
+use trust_dns_resolver::lookup_ip::LookupIp;
 use trust_dns_resolver::ResolverFuture;
-use trust_dns_resolver::lookup_ip::{LookupIpFuture, LookupIp};
-use trust_dns_resolver::error::{ResolveResult, ResolveError};
-
 
 fn main() {
-    let lookup_future = resolve("www.reddit.com").unwrap();
+    let lookup_future = resolve("www.google.com").unwrap();
     let mut io_loop = Runtime::new().expect("failed creating a runtime");
     let lookup_res = io_loop.block_on(lookup_future).unwrap();
     println!("resolve: {:?}", lookup_res);
 }
 
-pub fn resolve(host: &'static str) -> ResolveResult<Box<Future<Item=LookupIp, Error=ResolveError>>> {
+pub fn resolve(
+    host: &'static str,
+) -> ResolveResult<impl Future<Item = LookupIp, Error = ResolveError>> {
     let resolver = ResolverFuture::from_system_conf()?;
-    Ok(Box::new(resolver.and_then(move |r| r.lookup_ip(host))))
+    Ok(resolver.and_then(move |r| r.lookup_ip(host)))
 }
+
 
 // #[derive(Debug)]
 // struct Connecting {
