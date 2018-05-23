1fac3ba8e9fc9656a2371de6f0869994ec050510
Evan Simmons <evan.simmons@creditkarma.com>
2018-05-23 10:27:04 -0700

    Add config params

--- src/main.rs
+++ src/main.rs
@@ -24,64 +24,98 @@ use trust_dns_resolver::config::LookupIpStrategy;
 fn main() {
     env_logger::init();
     let mut io_loop = Runtime::new().expect("failed creating a runtime");
+    let dialer = Dialer::default();
 
-    let lookup_future = resolve("www.google.com").unwrap();
+    let lookup_future = dialer.resolve("www.google.com").unwrap();
     let lookup_res = io_loop.block_on(lookup_future).unwrap();
     println!("resolve: {:?}", lookup_res);
 
-    let dial_future = dial("www.google.com", 80).unwrap();
+    let dialer = Dialer::default();
+    let dial_future = dialer.dial("www.google.com", 80).unwrap();
     let dial_res = io_loop.block_on(dial_future).unwrap();
     println!("dial {:?}", dial_res);
 }
 
-pub fn resolve(
-    host: &'static str,
-) -> ResolveResult<impl Future<Item = LookupIp, Error = ResolveError>> {
-    debug!("resolving: {}", host);
-    let (conf, mut opts) = read_system_conf()?;
-    opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
-    let resolver = ResolverFuture::new(conf, opts);
-    Ok(resolver.and_then(move |r| r.lookup_ip(host)))
+/// Dialer uses happy eyeballs to resolve and connect to a host
+/// The struct fields are named according to the happy eyeballs
+/// rfc [rfc8305](https://tools.ietf.org/html/rfc8305#section-3)
+struct Dialer {
+    pub resolution_delay: u16,
+    pub first_addr_family_count: u8,
+    pub conn_attempt_delay: u16,
+    pub min_conn_delay: u16,
+    pub max_conn_delay: u16,
+    pub last_resort_local_synthesis_delay: u16,
+}
+
+/// Defaults as specified in the rfc
+impl Default for Dialer {
+    fn default() -> Self {
+        Self {
+            resolution_delay: 50, // unused
+            first_addr_family_count: 1, // unused
+            conn_attempt_delay: 250,
+            min_conn_delay: 100,
+            max_conn_delay: 2000,
+            last_resort_local_synthesis_delay: 2000,
+        }
+    }
+}
+
+impl Dialer {
+    pub fn dial(self, host: &'static str, port: u16) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
+        debug!("dialing: {}, {}", host, port);
+        let ip_fut = self.resolve(host)?;
+        let res = ip_fut
+            .map_err(|e| e.into())
+            .and_then(move |ips| {
+                let socks = ips.iter().map(move |x| SocketAddr::new(x, port)).collect::<Vec<SocketAddr>>();
+                self.happy_connect(socks.into_iter()).map_err(|e| e.into())
+            });
+        Ok(Box::new(res))
+    }
+    pub fn resolve(
+        &self,
+        host: &'static str,
+    ) -> ResolveResult<impl Future<Item = LookupIp, Error = ResolveError>> {
+        debug!("resolving: {}", host);
+        let (conf, mut opts) = read_system_conf()?;
+        opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
+        let resolver = ResolverFuture::new(conf, opts);
+        Ok(resolver.and_then(move |r| r.lookup_ip(host)))
+    }
+    fn happy_connect(&self, socks: impl Iterator<Item=SocketAddr>) -> Box<Future<Item = TcpStream, Error = Box<std::error::Error>>> {
+        let (tx, rx) = futures::sync::mpsc::channel(1);
+        for (i, s) in socks.enumerate() {
+            let offset = ((i as u64)+1) * self.conn_attempt_delay as u64;
+            let when = Instant::now() + Duration::from_millis(offset);
+            let txc = tx.clone();
+            debug!("creating delay of {}", offset);
+            let delay = Delay::new(when)
+                .map_err(|e| panic!("delay errored; err={:?}", e))
+                .and_then(move |_| {
+                    debug!("happy eyeballs start connect: {}", s);
+                    TcpStream::connect(&s)
+                        .map_err(|e| panic!("delay errored; err={:?}", e))
+                })
+                .and_then(move |c| {
+                    debug!("happy eyeballs connected: {:?}", c);
+                    txc.send(c).and_then(|_| Ok(()))
+                    .map_err(|e| panic!("delay errored; err={:?}", e))
+                });
+            tokio::spawn(delay);
+        }
+        Box::new(rx.into_future().map(|x| {
+            debug!("happy eyeballs recv: {:?}", x.0);
+            x.0.unwrap()
+        }).map_err(|e| panic!("delay errored; err={:?}", e)))
+    }
 }
 
 pub fn dial(host: &'static str, port: u16) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
-    debug!("dialing: {}, {}", host, port);
-    let ip_fut = resolve(host)?;
-    let res = ip_fut
-        .map_err(|e| e.into())
-        .and_then(move |ips| {
-            let socks = ips.iter().map(move |x| SocketAddr::new(x, port)).collect::<Vec<SocketAddr>>();
-            happy_connect(socks.into_iter()).map_err(|e| e.into())
-        });
-    Ok(Box::new(res))
+    Dialer::default().dial(host, port)
 }
 
-pub fn happy_connect(socks: impl Iterator<Item=SocketAddr>) -> Box<Future<Item = TcpStream, Error = Box<std::error::Error>>> {
-    let (tx, rx) = futures::sync::mpsc::channel(1);
-    for (i, s) in socks.enumerate() {
-        let offset = ((i as u64)+1) * 100;
-        let when = Instant::now() + Duration::from_millis(offset);
-        let txc = tx.clone();
-        debug!("creating delay of {}", offset);
-        let delay = Delay::new(when)
-            .map_err(|e| panic!("delay errored; err={:?}", e))
-            .and_then(move |_| {
-                debug!("happy eyeballs start connect: {}", s);
-                TcpStream::connect(&s)
-                    .map_err(|e| panic!("delay errored; err={:?}", e))
-            })
-            .and_then(move |c| {
-                debug!("happy eyeballs connected: {:?}", c);
-                txc.send(c).and_then(|_| Ok(()))
-                .map_err(|e| panic!("delay errored; err={:?}", e))
-            });
-        tokio::spawn(delay);
-    }
-    Box::new(rx.into_future().map(|x| {
-        debug!("happy eyeballs recv: {:?}", x.0);
-        x.0.unwrap()
-    }).map_err(|e| panic!("delay errored; err={:?}", e)))
-}
 
 
 
