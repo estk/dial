4a9677b4717ba186e6b356cb1e224308844d0301
Evan Simmons <evan.simmons@creditkarma.com>
2018-05-26 13:32:48 -0700

    reorder stream.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,37 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 extern crate futures;
 extern crate tokio;
 extern crate tokio_tcp;
 extern crate trust_dns_resolver;
 
+use futures::sync::oneshot;
+use std::net::IpAddr;
+use trust_dns_resolver::lookup_ip::LookupIp;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::net::SocketAddr;
 use std::time::{Duration, Instant};
+use tokio::prelude::stream::futures_unordered;
 
 use futures::Future;
 
 use tokio::prelude::*;
-use tokio::timer::Delay;
 use tokio::runtime::current_thread::Runtime;
+use tokio::timer::Delay;
 use tokio_tcp::TcpStream;
 
+use trust_dns_resolver::config::LookupIpStrategy;
 use trust_dns_resolver::error::{ResolveError, ResolveResult};
-use trust_dns_resolver::lookup_ip::LookupIp;
 use trust_dns_resolver::system_conf::read_system_conf;
 use trust_dns_resolver::ResolverFuture;
-use trust_dns_resolver::config::LookupIpStrategy;
 
 fn main() {
     env_logger::init();
     let mut io_loop = Runtime::new().expect("failed creating a runtime");
-    let dialer = Dialer::default();
-
-    let lookup_future = dialer.resolve("www.google.com").unwrap();
-    let lookup_res = io_loop.block_on(lookup_future).unwrap();
-    println!("resolve: {:?}", lookup_res);
+    // let lookup_future = dialer.resolve("www.google.com").unwrap();
+    // let lookup_res = io_loop.block_on(lookup_future).unwrap();
+    // println!("resolve: {:?}", lookup_res);
 
     let dialer = Dialer::default();
     let dial_future = dialer.dial("www.google.com", 80).unwrap();
@@ -36,10 +39,19 @@ fn main() {
     println!("dial {:?}", dial_res);
 }
 
+pub fn dial(
+    host: &'static str,
+    port: u16,
+) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
+    Dialer::default().dial(host, port)
+}
+
 /// Dialer uses happy eyeballs to resolve and connect to a host
 /// The struct fields are named according to the happy eyeballs
 /// rfc [rfc8305](https://tools.ietf.org/html/rfc8305#section-3)
+#[derive(Debug, Clone, Copy)]
 struct Dialer {
+    pub prefer_v6: bool,
     pub resolution_delay: u16,
     pub first_addr_family_count: u8,
     pub conn_attempt_delay: u16,
@@ -52,82 +64,123 @@ struct Dialer {
 impl Default for Dialer {
     fn default() -> Self {
         Self {
-            resolution_delay: 50, // unused
+            prefer_v6: true,
+            resolution_delay: 50,       // unused
             first_addr_family_count: 1, // unused
             conn_attempt_delay: 250,
-            min_conn_delay: 100, // unused
-            max_conn_delay: 2000, // unused
+            min_conn_delay: 100,                     // unused
+            max_conn_delay: 2000,                    // unused
             last_resort_local_synthesis_delay: 2000, // unused
         }
     }
 }
 
-    pub fn dial<T: AsRef<Dialer>>(dialer: T, host: &'static str, port: u16) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
-        debug!("dialing: {}, {}", host, port);
-        let ip_fut = dialer.as_ref().resolve(host)?;
-        let res = ip_fut
-            .map_err(|e| e.into())
-            .and_then(move |ips| {
-                let socks = ips.iter().map(move |x| SocketAddr::new(x, port)).collect::<Vec<SocketAddr>>();
-                dialer.as_ref().happy_connect(socks.into_iter()).map_err(|e| e.into())
-            });
-        Ok(Box::new(res))
-    }
-
 impl Dialer {
-    pub fn dial(&self, host: &'static str, port: u16) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
-        let dialer = self.as_ref();
+    pub fn dial(
+        self,
+        host: &'static str,
+        port: u16,
+    ) -> ResolveResult<impl Future<Item = TcpStream, Error = Box<std::error::Error>>> {
         debug!("dialing: {}, {}", host, port);
-        let ip_fut = self.resolve(host)?;
-        let res = ip_fut
-            .map_err(|e| e.into())
-            .and_then(move |ips| {
-                let socks = ips.iter().map(move |x| SocketAddr::new(x, port)).collect::<Vec<SocketAddr>>();
-                self.happy_connect(socks.into_iter()).map_err(|e| e.into())
-            });
-        Ok(Box::new(res))
+
+        // resolve should be a stream
+        let (v4, v6) = self.resolve(host)?;
+        let ordered = self.order(v4, v6);
+        let socks = ordered
+            .map(|ip| SocketAddr::new(ip, port));
+
+        Ok(Box::new(self.connect(socks)))
+    }
+    fn order(
+        &self,
+        v4: impl Future<Item = LookupIp, Error = ResolveError> + Send,
+        v6: impl Future<Item = LookupIp, Error = ResolveError> + Send,
+    ) -> impl Stream<Item = IpAddr, Error = ()> {
+        let (tx, rx) = futures::sync::mpsc::channel(1);
+        let (unblock_v4, blocked_v4) = oneshot::channel();
+        let (unblock_v6, blocked_v6) = oneshot::channel();
+        if self.prefer_v6 {
+            unblock_v6.send(());
+        } else {
+            unblock_v4.send(());
+        }
+        tokio::spawn(v4
+            .map_err(|e| eprintln!("accept failed = {:?}", e))
+            .and_then(|lip| {
+                unblock_v6.send(());
+                blocked_v4.and_then(|_| {
+                    stream::iter_ok(lip.iter()).and_then(|ip| {
+                        tx.send(ip)
+                })
+                .into()
+            })
+        }));
+        tokio::spawn(v6
+            .map_err(|e| eprintln!("accept failed = {:?}", e))
+            .and_then(|lip| {
+                unblock_v4.send(());
+                blocked_v6.and_then(|_| {
+                    stream::iter_ok(lip.iter()).and_then(|ip| {
+                        tx.send(ip)
+                    }).into()
+                })
+        }));
+        // interleave v4 and v6
+
+        rx
     }
     pub fn resolve(
         &self,
         host: &'static str,
-    ) -> ResolveResult<impl Future<Item = LookupIp, Error = ResolveError>> {
+    ) -> ResolveResult<(impl Future<Item = LookupIp, Error = ResolveError>, impl Future<Item = LookupIp, Error = ResolveError>)> {
         debug!("resolving: {}", host);
-        let (conf, mut opts) = read_system_conf()?;
-        opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
-        let resolver = ResolverFuture::new(conf, opts);
-        Ok(resolver.and_then(move |r| r.lookup_ip(host)))
+        let (conf, opts) = read_system_conf()?;
+        // read_system_conf just returns the ::default opts
+        let mut v4_opts = opts.clone();
+        v4_opts.ip_strategy = LookupIpStrategy::Ipv4Only;
+        let v4_fut = ResolverFuture::new(conf.clone(), v4_opts)
+            .and_then(|r| r.lookup_ip(host));
+
+        let mut v6_opts = opts.clone();
+        v6_opts.ip_strategy = LookupIpStrategy::Ipv6Only;
+        let v6_fut = ResolverFuture::new(conf.clone(), v6_opts)
+            .and_then(|r| r.lookup_ip(host));
+
+        Ok((v4_fut, v6_fut))
     }
-    fn happy_connect(&self, socks: impl Iterator<Item=SocketAddr>) -> Box<Future<Item = TcpStream, Error = Box<std::error::Error>>> {
+    pub fn connect(
+        &self,
+        socks: impl Stream<Item = SocketAddr, Error = ResolveError>,
+
+    ) -> Box<Future<Item = TcpStream, Error = Box<std::error::Error>>> {
+        debug!("Connect");
+        // let now = Instant::now();
         let (tx, rx) = futures::sync::mpsc::channel(1);
-        for (i, s) in socks.enumerate() {
-            let offset = ((i as u64)+1) * self.conn_attempt_delay as u64;
+        stream::iter_ok(1..).zip(socks).for_each(|(i, s)| {
+            let offset = ((i as u64) + 1) * self.conn_attempt_delay as u64;
             let when = Instant::now() + Duration::from_millis(offset);
             let txc = tx.clone();
             debug!("creating delay of {}", offset);
-            let delay = Delay::new(when)
+            Delay::new(when)
                 .map_err(|e| panic!("delay errored; err={:?}", e))
                 .and_then(move |_| {
-                    debug!("happy eyeballs start connect: {}", s);
-                    TcpStream::connect(&s)
-                        .map_err(|e| panic!("delay errored; err={:?}", e))
+                    debug!("[]happy eyeballs start connect: {}", s);
+                    TcpStream::connect(&s).map_err(|e| panic!("delay errored; err={:?}", e))
                 })
                 .and_then(move |c| {
                     debug!("happy eyeballs connected: {:?}", c);
-                    txc.send(c).and_then(|_| Ok(()))
-                    .map_err(|e| panic!("delay errored; err={:?}", e))
-                });
-            tokio::spawn(delay);
-        }
-        Box::new(rx.into_future().map(|x| {
-            debug!("happy eyeballs recv: {:?}", x.0);
-            x.0.unwrap()
-        }).map_err(|e| panic!("delay errored; err={:?}", e)))
+                    txc.send(c)
+                        .and_then(|_| Ok(()))
+                        .map_err(|e| panic!("delay errored; err={:?}", e))
+                })
+        });
+        Box::new(
+            rx.into_future()
+                .map(|x| {
+                    debug!("happy eyeballs recv: {:?}", x.0);
+                    x.0.unwrap()
+                })
+                .map_err(|e| panic!("delay errored; err={:?}", e)),
+        )
     }
 }
-
-
-
-
-
-
-    //
